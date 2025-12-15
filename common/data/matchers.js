b62e8689a8f1159d484fa6ccc34092b5e4596dc1
ThePieBandit <61213359+ThePieBandit@users.noreply.github.com>
2025-12-15 12:55:30 +0000

    Automatic publish syncing the latest from OPTC-DB

--- common/data/matchers.js
+++ common/data/matchers.js
@@ -12880,6 +12880,46 @@
 			},
 
 			{
+				name: "Half Stats",
+				targets: ["rumbleSpecial"],
+				regex:
+					/([.\d]+)% chance to evade[^.]+Half Stats[^.]+to (self|(?=((?:[^c]+|c(?!rew))*))\3crew members?)(?:, excluding self,)?(?: with [^.]+ (ATK|DEF|HP|RCV|SPD|Special CT))?(?: for (\d+) seconds)?/i,
+				submatchers: [
+					{
+						type: "number",
+						description: "Chance:",
+						groups: [1],
+					},
+					{
+						type: "number",
+						description: "Duration:",
+						groups: [5],
+					},
+					{
+						type: "separator",
+						description: "Targeting:",
+					},
+					{
+						type: "option",
+						description: "Universal",
+						regex: /all/i,
+						groups: [2],
+						cssClasses: ["min-width-6"],
+					},
+					{
+						type: "separator",
+						description: "Types:",
+					},
+					...createTypesSubmatchers([2]),
+					{
+						type: "separator",
+						description: "Classes:",
+					},
+					...createClassesSubmatchers([2]),
+				],
+			},
+
+			{
 				name: "Paralysis",
 				targets: ["rumbleSpecial"],
 				regex:
